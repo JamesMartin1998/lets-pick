a3d1b88d47092784ffb953ff4e5302b192b527ac
JamesMartin1998 <jamesandrewmartin1998@hotmail.co.uk>
2023-01-24 11:59:22 +0000

    add basic NavBar component and style#

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,19 +1,19 @@
 import React from "react";
 import { Navbar, Container, Nav } from "react-bootstrap";
 import logo from "../assets/logo.png"
+import styles from "../styles/NavBar.module.css";
 
 const NavBar = () => {
   return (
-    <Navbar bg="light" expand="md" fixed="top">
+    <Navbar className={styles.NavBar} expand="md" fixed="top">
       <Container>
         <Navbar.Brand><img src={logo} alt="logo" height="45" /></Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ml-auto">
-            <Nav.Link href="#home"><i className="fas fa-home"></i>Home</Nav.Link>
-            <Nav.Link href="#link"><i className="fas fa-sign-in"></i>Sign In</Nav.Link>
-            <Nav.Link href="#link"><i className="fas fa-user-plus"></i>Sign Up</Nav.Link>
-            <Nav.Link href="#link">Sign In</Nav.Link>
+            <Nav.Link className={styles.NavBarLinks} href="#home"><i className="fas fa-home"></i>Home</Nav.Link>
+            <Nav.Link className={styles.NavBarLinks} href="#link"><i className="fas fa-sign-in"></i>Sign In</Nav.Link>
+            <Nav.Link className={styles.NavBarLinks} href="#link"><i className="fas fa-user-plus"></i>Sign Up</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
