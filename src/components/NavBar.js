44f0ebcdee84cb70252bfafc11fba0178206f6c0
JamesMartin1998 <jamesandrewmartin1998@hotmail.co.uk>
2023-01-26 15:50:13 +0000

    add more loggedInIcons to NavBar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -7,6 +7,17 @@ import { useCurrentUser } from "../contexts/CurrentUserContext";
 
 const NavBar = () => {
   const currentUser = useCurrentUser();
+
+  const addPostIcon = (
+    <NavLink
+      to="/posts/create"
+      className={styles.NavLink}
+      activeClassName={styles.Active}
+    >
+      <i className="far fa-plus-square"></i>Add Post
+    </NavLink>
+  );
+
   const loggedOutIcons = (
     <>
       <NavLink
@@ -28,9 +39,35 @@ const NavBar = () => {
 
   const loggedInIcons = (
     <>
-      {currentUser?.username}
+      <NavLink
+        to="/yourvotes"
+        className={styles.NavLink}
+        activeClassName={styles.Active}
+      >
+        <i class="fa-solid fa-check-to-slot"></i>Your Votes
+      </NavLink>
+      <NavLink
+        to="/favourites"
+        className={styles.NavLink}
+        activeClassName={styles.Active}
+      >
+        <i class="fa-solid fa-star"></i>Favourites
+      </NavLink>
+      <NavLink
+        to={`/profiles/${currentUser?.profile_id}`}
+        className={styles.NavLink}
+      >
+        <img src={currentUser?.profile_image} />
+      </NavLink>
+      <NavLink
+        to="/"
+        className={styles.NavLink}
+        onClick={()=>{}}
+      >
+        <i class="fa-solid fa-door-closed"></i>Sign Out
+      </NavLink>
     </>
-  )
+  );
 
   return (
     <Navbar className={styles.NavBar} expand="md" fixed="top">
@@ -40,6 +77,7 @@ const NavBar = () => {
             <img src={logo} alt="logo" height="45" />
           </Navbar.Brand>
         </NavLink>
+        {currentUser && addPostIcon}
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ml-auto">
