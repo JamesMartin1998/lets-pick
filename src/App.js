2078ec41b6a3558b1a1c5b998de41abeb1b197dd
JamesMartin1998 <jamesandrewmartin1998@hotmail.co.uk>
2023-01-26 11:03:35 +0000

    add CurrentUserContext and SetCurrentUserContext to pass variables to child components

--- src/App.js
+++ src/App.js
@@ -8,10 +8,14 @@ import SignInForm from './pages/auth/SignInForm';
 import { useEffect, useState } from 'react';
 import axios from 'axios';
 
+export const CurrentUserContext = createContext();
+export const SetCurrentUserContext = createContext();
+
 function App() {
 
   const [currentUser, setCurrentUser] = useState(null);
 
+  // gets logged in user data
   const handleMount = async () => {
     try {
       await axios.get('dj-rest-auth/user/')
@@ -20,23 +24,29 @@ function App() {
     }
   }
 
+  // calls the handleMount function once the components have mounted
   useEffect(() => {
     handleMount()
   }, [])
 
 
   return (
-    <div className={styles.App}>
-      <NavBar />
-      <Container className={styles.Main}>
-        <Switch>
-          <Route exact path="/" render={()=> <h1>Home Page</h1>} />
-          <Route exact path="/signin" render={()=> <SignInForm />} />
-          <Route exact path="/signup" render={()=> <SignUpForm />} />
-          <Route render={()=> <p>Page not found</p>} />
-        </Switch>
-      </Container>
-    </div>
+    // Context used to pass the currentUser and setCurrentUser variables to all chidlren components
+    <CurrentUserContext.Provider value={currentUser}>
+      <SetCurrentUserContext.Provider value={setCurrentUser}>
+        <div className={styles.App}>
+          <NavBar />
+          <Container className={styles.Main}>
+            <Switch>
+              <Route exact path="/" render={()=> <h1>Home Page</h1>} />
+              <Route exact path="/signin" render={()=> <SignInForm />} />
+              <Route exact path="/signup" render={()=> <SignUpForm />} />
+              <Route render={()=> <p>Page not found</p>} />
+            </Switch>
+          </Container>
+        </div>
+      </SetCurrentUserContext.Provider>
+    </CurrentUserContext.Provider>
   );
 }
 
