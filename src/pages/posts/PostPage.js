2e67d8c1161e37594ae02549a940fb8368de535c
JamesMartin1998 <jamesandrewmartin1998@hotmail.co.uk>
2023-01-28 11:57:48 +0000

    add PostPage component with handleMount function to retrieve post data from API

---
+++ src/pages/posts/PostPage.js
@@ -0,0 +1,44 @@
+import React, { useEffect, useState } from "react";
+
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
+import Container from "react-bootstrap/Container";
+
+import appStyles from "../../App.module.css";
+import { useParams } from "react-router";
+import { axiosReq } from "../../api/axiosDefaults";
+
+function PostPage() {
+  const { id } = useParams();
+
+  const [post, setPost] = useState({ results: [] });
+
+//   get post data from API
+  useEffect(() => {
+    const handleMount = async () => {
+      try {
+        // destructure data and rename to post
+        const [{data: post}] = await Promise.all([
+            axiosReq.get(`/posts/${id}`),
+        ])
+        setPost({results: [post]})
+        console.log(post)
+      } catch (err) {
+        console.log(err)
+      }
+    };
+
+    handleMount()
+  }, [id]);
+
+  return (
+    <Row className="h-100">
+      <Col className="py-2 p-0 p-lg-2" lg={12}>
+        <p>Post component</p>
+        <Container className={appStyles.Content}>Comments</Container>
+      </Col>
+    </Row>
+  );
+}
+
+export default PostPage;
