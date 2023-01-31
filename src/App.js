f44b2b819ecbe2885a29af4cc5fd2819ac6a09bc
JamesMartin1998 <jamesandrewmartin1998@hotmail.co.uk>
2023-01-31 15:49:18 +0000

    add routes to display the PostsPage component on the homepage, yourvotes page and favourites page

--- src/App.js
+++ src/App.js
@@ -7,14 +7,47 @@ import SignUpForm from "./pages/auth/SignUpForm";
 import SignInForm from "./pages/auth/SignInForm";
 import PostCreateForm from "./pages/posts/PostCreateForm";
 import PostPage from "./pages/posts/PostPage";
+import PostsPage from "./pages/posts/PostsPage";
+import { useCurrentUser } from "./contexts/CurrentUserContext";
 
 function App() {
+  const currentUser = useCurrentUser();
+  const profile_id = currentUser?.profile_id || "";
+
   return (
     <div className={styles.App}>
       <NavBar />
       <Container className={styles.Main}>
         <Switch>
-          <Route exact path="/" render={() => <h1>Home Page</h1>} />
+          <Route
+            exact
+            path="/"
+            render={() => (
+              <PostsPage message="Your search returned no results" />
+            )}
+          />
+          <Route
+            exact
+            path="/yourvotes"
+            render={() => (
+              <PostsPage
+                message="No results found. Did you vote on any posts?"
+                // need to check the filter string
+                filter={`votes__owner__profile=${profile_id}&`}
+              />
+            )}
+          />
+          <Route
+            exact
+            path="/favourites"
+            render={() => (
+              <PostsPage
+                message="No results found. Did you favourite any posts?"
+                // need to check the filter string
+                filter={`favourites__owner__profile=${profile_id}&ordering=-favourites__created_at&`}
+              />
+            )}
+          />
           <Route exact path="/signin" render={() => <SignInForm />} />
           <Route exact path="/signup" render={() => <SignUpForm />} />
           <Route exact path="/posts/create" render={() => <PostCreateForm />} />
@@ -27,4 +60,3 @@ function App() {
 }
 
 export default App;
-
