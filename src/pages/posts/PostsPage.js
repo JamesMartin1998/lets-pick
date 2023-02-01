68ed791c56f05186003e19edfcd4a7004e0be31a
JamesMartin1998 <jamesandrewmartin1998@hotmail.co.uk>
2023-02-01 10:55:31 +0000

    add PostsPage component to render posts that are filtered for the different pages

--- src/pages/posts/PostsPage.js
+++ src/pages/posts/PostsPage.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 import Form from "react-bootstrap/Form";
 import Col from "react-bootstrap/Col";
@@ -7,17 +7,54 @@ import Container from "react-bootstrap/Container";
 
 import appStyles from "../../App.module.css";
 import styles from "../../styles/PostsPage.module.css";
+import { useLocation } from "react-router-dom";
+import { axiosReq } from "../../api/axiosDefaults";
+import Post from "./Post";
 
-function PostsPage() {
+import NoResults from "../../assets/no-results.png"
+import Asset from "../../components/Asset";
+
+function PostsPage({message, filter=""}) {
+
+  const [posts, setPosts] = useState({results: []})
+  const [hasLoaded, setHasLoaded] = useState(false)
+  const {pathname} = useLocation()
+
+  useEffect(() => {
+    // different posts will be fetched on the home page, your votes page and favoruites page
+    // due to the filter prop
+    const fetchPosts = async () => {
+      try {
+        const {data} = await axiosReq.get(`/posts/?${filter}`)
+        setPosts(data)
+        setHasLoaded(true)
+      } catch(err) {
+        console.log(err)
+      }
+    }
+    setHasLoaded(false)
+    fetchPosts()
+  }, [filter, pathname])
   
   return (
     <Row className="h-100">
-      <Col className="py-2 p-0 p-lg-2" lg={8}>
+      <Col className="py-2 p-0 p-lg-2" lg={12}>
         <p>Popular profiles mobile</p>
-        <p>List of posts here</p>
-      </Col>
-      <Col md={4} className="d-none d-lg-block p-0 p-lg-2">
-        <p>Popular profiles for desktop</p>
+        {hasLoaded ? (
+          <>
+            {posts.results.length ? (
+              posts.results.map((post) => (
+                <Post key={post.id} {...post} setPosts={setPosts} />
+              ))
+            ) : (
+              <Container>
+                <Asset src={NoResults} message={message} />
+              </Container>
+            )}
+          </>
+        ) : (
+          <Asset spinner />
+        )}
       </Col>
     </Row>
   );
