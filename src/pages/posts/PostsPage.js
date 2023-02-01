de178ca944adb6313d13772dc8032518b4a50f55
JamesMartin1998 <jamesandrewmartin1998@hotmail.co.uk>
2023-02-01 15:14:29 +0000

    add setTimeOut function in the useEffect hook on the PostsPage so that the search bar waits 1 second after typing to fetch the posts

--- src/pages/posts/PostsPage.js
+++ src/pages/posts/PostsPage.js
@@ -11,52 +11,52 @@ import { useLocation } from "react-router-dom";
 import { axiosReq } from "../../api/axiosDefaults";
 import Post from "./Post";
 
-import NoResults from "../../assets/no-results.png"
+import NoResults from "../../assets/no-results.png";
 import Asset from "../../components/Asset";
 
-function PostsPage({message, filter=""}) {
-
-  const [posts, setPosts] = useState({results: []})
-  const [hasLoaded, setHasLoaded] = useState(false)
-  const {pathname} = useLocation()
-  const [query, setQuery] = useState("")
+function PostsPage({ message, filter = "" }) {
+  const [posts, setPosts] = useState({ results: [] });
+  const [hasLoaded, setHasLoaded] = useState(false);
+  const { pathname } = useLocation();
+  const [query, setQuery] = useState("");
 
   useEffect(() => {
     // different posts will be fetched on the home page, your votes page and favoruites page
     // due to the filter prop
     const fetchPosts = async () => {
       try {
-        const {data} = await axiosReq.get(`/posts/?${filter}search=${query}`)
-        setPosts(data)
-        setHasLoaded(true)
-      } catch(err) {
-        console.log(err)
+        const { data } = await axiosReq.get(`/posts/?${filter}search=${query}`);
+        setPosts(data);
+        setHasLoaded(true);
+      } catch (err) {
+        console.log(err);
       }
-    }
-    setHasLoaded(false)
-    fetchPosts()
-  }, [filter, pathname, query])
-  
+    };
+    setHasLoaded(false);
+    const timer = setTimeout(() => {
+      fetchPosts();
+    }, 1000);
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [filter, pathname, query]);
+
   return (
     <Row className="h-100">
       <Col className="py-2 p-0 p-lg-2" lg={12}>
-        <p>Popular profiles mobile</p>
-
         <i className={`fas fa-search ${styles.SearchIcon}`} />
         <Form
           className={`${styles.SearchBar}`}
           onSubmit={(event) => event.preventDefault()}
         >
-        <Form.Control
-          value={query}
-          onChange={(event) => setQuery(event.target.value)}
-          type="text"
-          className="mr-sm-2"
-          placeholder="Search Posts"
-        />
+          <Form.Control
+            value={query}
+            onChange={(event) => setQuery(event.target.value)}
+            type="text"
+            className="mr-sm-2"
+            placeholder="Search Posts"
+          />
         </Form>
-
-
 
         {hasLoaded ? (
           <>
