76b17ab7c641062e43bbd3c4576115c72ae680e8
JamesMartin1998 <jamesandrewmartin1998@hotmail.co.uk>
2023-02-03 10:32:04 +0000

    add CommentCreateForm to allow users to comment on post

--- src/pages/posts/PostPage.js
+++ src/pages/posts/PostPage.js
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from "react";
-
+import CommentCreateForm from "../comments/CommentCreateForm";
+import { useCurrentUser } from "../../contexts/CurrentUserContext";
+import styles from "../../styles/PostPage.module.css"
 import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
 import Container from "react-bootstrap/Container";
@@ -14,29 +16,45 @@ function PostPage() {
 
   const [post, setPost] = useState({ results: [] });
 
-//   get post data from API
+  const currentUser = useCurrentUser();
+  const profile_image = currentUser?.profile_image;
+  const [comments, setComments] = useState({ results: [] });
+
+  //   get post data from API
   useEffect(() => {
     const handleMount = async () => {
       try {
         // destructure data and rename to post
-        const [{data: post}] = await Promise.all([
-            axiosReq.get(`/posts/${id}`),
-        ])
-        setPost({results: [post]})
-        console.log(post)
+        const [{ data: post }] = await Promise.all([
+          axiosReq.get(`/posts/${id}`),
+        ]);
+        setPost({ results: [post] });
+        console.log(post);
       } catch (err) {
-        console.log(err)
+        console.log(err);
       }
     };
 
-    handleMount()
+    handleMount();
   }, [id]);
 
   return (
     <Row className="h-100">
       <Col className="py-2 p-0 p-lg-2" lg={12}>
         <Post {...post.results[0]} setPosts={setPost} postPage />
-        <Container className={appStyles.Content}>Comments</Container>
+        <Container className={styles.Container}>
+          {currentUser ? (
+            <CommentCreateForm
+              profile_id={currentUser.profile_id}
+              profileImage={profile_image}
+              post={id}
+              setPost={setPost}
+              setComments={setComments}
+            />
+          ) : comments.results.length ? (
+            "Comments"
+          ) : null}
+        </Container>
       </Col>
     </Row>
   );
