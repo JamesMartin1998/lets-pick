cb8362e5f2f862a24cd746e439c5be761bae0ae9
JamesMartin1998 <jamesandrewmartin1998@hotmail.co.uk>
2023-02-07 15:34:59 +0000

    add useRedirect hook to redirect users to the home page

---
+++ src/hooks/useRedirect.js
@@ -0,0 +1,26 @@
+import axios from "axios";
+import { useEffect } from "react";
+import { useHistory } from "react-router"
+
+export const useRedirect = (userAuthStatus) => {
+    const history = useHistory();
+
+    useEffect(() => {
+        const handleMount = async () => {
+            try {
+                // checks if user is logged in or not
+                await axios.post('/dj-rest-auth/token/refresh/')
+                // code below only runs if logged in
+                if (userAuthStatus === 'loggedIn') {
+                    history.push('/')
+                }
+            } catch (err) {
+                // code below runs if user is logged out
+                if (userAuthStatus === 'loggedOut') {
+                    history.push('/')
+                }
+            }
+        }
+        handleMount()
+    }, [history, userAuthStatus])
+}
